ee0cd66b100390324e6ecd2bb319c5f82d635292
Alexey Bezrukov <2choirboy@gmail.com>
2015-03-31 11:10:22 +1000

    ~~~~

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @omniauth = env['omniauth.auth']
     @credential = Credential.where(provider: @omniauth.provider, uid: @omniauth.uid).first
     current_user ? social_network_data_processing : find_or_create_user
-    @user.persisted? ? sign_in_or_redirect : sign_in_new_user
+    @user.save unless @user.persisted?
+    sign_in @user
+    redirect_to edit_user_registration_path
   end
 
   def failure
@@ -22,7 +24,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def find_or_create_user
     @credential ? find_user : create_user
   end
-
+#TODO case : есть пользователь который зарегистрировался по email, а после кто то заходит через соц сеть с таким же email-ом.
   def create_user
     @user = User.create_user_from_omniauth(@omniauth)
   end
@@ -44,16 +46,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def wrong_social_network_data
     @user = @credential.user
     sign_out current_user
-  end
-
-  def sign_in_new_user
-    @user.save
-    sign_in @user
-    redirect_to edit_user_registration_path
-  end
-
-  def sign_in_or_redirect
-    current_user ? redirect_to(edit_user_registration_path) : sign_in_and_redirect(@user)
   end
 
   def unbind
